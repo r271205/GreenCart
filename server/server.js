10a26de553aaae52600a88ff59a63d233e4e3d6c
r271205 <bhattirajan007@gmail.com>
2026-02-13 17:14:55 +0530

    Update allowed origins for CORS configuration

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB();
 await connectCloudinary()
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://green-cart-sooty-gamma.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://green-cart-ikje.vercel.app/']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
